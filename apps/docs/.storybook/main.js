0b33e355288111d9643309480e758b3996f41e33
treckstar <treckstar@gmail.com>
2023-04-23 22:51:49 -0400

    Expose public folder to Storybook as static and add PostCSS and dark mode addons with SASS and Tailwind CSS support in main.js and preview.js, respectively, and update dependencies and configurations for webpack, Babel, PostCSS, and Tailwind CSS in package.json, postcss.config.js, and tailwind.config.js

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -2,26 +2,62 @@ const path = require("path");
 
 module.exports = {
   stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
-  },
-  async viteFinal(config, { configType }) {
-    // customize the Vite config here
-    return {
-      ...config,
-      resolve: {
-        alias: [
-          {
-            find: "@turbods/core",
-            replacement: path.resolve(
-              __dirname,
-              "../../../packages/turbods-core/"
-            ),
-          },
-        ],
+  /** Expose public folder to storybook as static */
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-dark-mode",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
       },
-    };
+    },
+  ],
+  core: {
+    builder: "webpack5",
+  },
+  framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    // SASS + Tailwdind CSS
+    config.module.rules.push({
+      test: /\.s(a|c)ss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1, // We always need to apply postcss-loader before css-loader
+            modules: {
+              auto: /\.module\.scss$/, // true
+              localIdentName: "[name]__[local]--[hash:base64:5]",
+            },
+          },
+        },
+        {
+          loader: "postcss-loader", // required for tailwind
+          options: {
+            implementation: require("postcss"), // postcss 8
+            postcssOptions: {
+              config: path.resolve(__dirname, "../postcss.config.js"),
+            },
+          },
+        },
+        {
+          loader: "sass-loader",
+          options: {
+            // sourceMap: true,
+            implementation: require("sass"), // dart sass
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
